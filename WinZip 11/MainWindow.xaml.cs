df98fa82b4bc9eab1779563ad6c1befa878112ae
Leapense <iso9661@gmail.com>
2022-05-12 21:11:19 +0900

    Add project files.

---
+++ WinZip 11/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Forms;
+using MessageBox = System.Windows.Forms.MessageBox;
+using System.Diagnostics;
+
+namespace WinZip_11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static string zip_file_name;
+        public static string destination_path;
+        public static string[] selected_files;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /// <summary>
+            /// Select Zipped File Method
+            /// </summary>
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Zip Files (*.zip)|*.zip|7z Files (*.7z)|*.7z|All Files (*.*)|*.*";
+            ofd.Title = "Select your zipped file.";
+            
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                zip_file_name = ofd.FileName;
+                FileN.Text = Path.GetFullPath(zip_file_name);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if(fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                destination_path = fbd.SelectedPath;
+                FolderN.Text = Path.GetFullPath(destination_path);
+            }
+        }
+
+        private void FolderN_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(FolderN.Text.ToLower().Equals("con/con") || FolderN.Text.ToLower().Equals("aux/aux") || FolderN.Text.ToLower().Equals("prn/prn"))
+            {
+                NoEaster.Content = "Ayyy!! You found the error of Windows 98!";
+                Secret s = new Secret();
+                s.Show();
+            }
+            else if(FolderN.Text.ToLower().Contains("porn"))
+            {
+                NoEaster.Content = "Police! Police! Arrest this horny person!";
+            }
+            else if (FolderN.Text.ToLower().Contains("egg"))
+            {
+                NoEaster.Content = "Why you storing egg on this computer?";
+            }
+            else
+            {
+                NoEaster.Content = "P.S. There is no easter-egg";
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            // TODO Make Extract Function
+            TB1.Header = "     Extract Compressed(Zipped) Folders";
+            Head.Content = "Select a Destination and Extract Files";
+            Body1.Content = "Select compressed file:";
+            FileN.Placeholder = "Select your Zip File or Enter the full path";
+            Body2.Content = "Files will be extracted to this folder:";
+            FolderN.Placeholder = "Select your Destination Path";
+            Body2.Visibility = Visibility.Visible;
+            FolderN.Visibility = Visibility.Visible;
+            Button3.Visibility = Visibility.Hidden;
+            Button4.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            // TODO Make Archive Function
+            TB1.Header = "     Archive Files";
+            Head.Content = "Select Files and Archive Folder Name";
+            Body1.Content = "Please Choose one or multiple files.";
+            Button3.Visibility = Visibility.Visible;
+            Button4.Visibility = Visibility.Visible;
+            Body2.Visibility = Visibility.Hidden;
+            FolderN.Placeholder = "Enter the folder path";
+        }
+        
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                selected_files = ofd.FileNames;
+            }
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Save Zip File";
+            sfd.Filter = "Zip Files (*.zip)|*.zip|7z Files (*.7z)|*.7z|All Files (*.*)|*.*";
+            if(sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string archive_path = Path.GetFullPath(sfd.FileName);
+                using(ZipArchive archive = ZipFile.Open(archive_path, ZipArchiveMode.Update))
+                {
+                    foreach(string file in selected_files)
+                    {
+                        archive.CreateEntryFromFile(file, Path.GetFileName(file));
+                    }
+                }
+            }
+            
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            ZipFile.ExtractToDirectory(FileN.Text, FolderN.Text);
+            if(TS1.IsChecked == true)
+            {
+                if(Directory.Exists(FolderN.Text))
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        Arguments = FolderN.Text,
+                        FileName = "explorer.exe"
+                    };
+                    Process.Start(startInfo);
+                }
+            }
+        }
+    }
+}
---
+++ WinZip 11/Secret.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WinZip_11
+{
+    partial class Secret
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Secret));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(795, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "System is busy.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkBlue;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(451, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(912, 280);
+            this.label2.TabIndex = 1;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(357, 757);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1109, 72);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Looks like you found the easteregg, You can click on this button to close.";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WinZip_11.Properties.Resources._21_211097_white_classic_windows_logo_by_mattatobin_d4s0i9j_microsoft;
+            this.pictureBox1.Location = new System.Drawing.Point(650, 954);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 93);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkBlue;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(754, 965);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(410, 74);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Windows 98 se";
+            // 
+            // Secret
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.ControlBox = false;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Secret";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Secret";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
