033b8f8e982e0414b18a1563eab06bf705cd9508
Leapense <Leapense@sillain.ac.kr>
2022-06-10 14:43:13 +0900

    Update v1.1.0

---
+++ WinZip 11/FileAlreadyExists.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MicaWPF.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WinZip_11
+{
+    /// <summary>
+    /// Interaction logic for FileAlreadyExists.xaml
+    /// </summary>
+    public partial class FileAlreadyExists : MicaWindow
+    {
+        public FileAlreadyExists()
+        {
+            InitializeComponent();
+            FolderPath.Content = MainWindow.file_exists_path;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                Arguments = MainWindow.file_exists_path,
+                FileName = "explorer.exe"
+            };
+            Process.Start(startInfo);
+            Close();
+        }
+    }
+}
--- WinZip 11/MainWindow.xaml.cs
+++ WinZip 11/MainWindow.xaml.cs
@@ -14,7 +14,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Forms;
-using MessageBox = System.Windows.Forms.MessageBox;
 using System.Diagnostics;
 using MicaWPF.Controls;
 using MicaWPF.Services;
@@ -36,27 +35,61 @@ namespace WinZip_11
                   WPFUI.Appearance.BackgroundType.Mica, // Background type
                   true                            // Whether to change accents automatically
                 );
-
+                Random random = new Random();
+                int tip_num = random.Next(0, win_fun_fact_list.Length);
+                WinFunFact.Content = win_fun_fact_list[tip_num];
             };
         }
 
-        public static string zip_file_name;
+        public static string[] zip_file_name;
         public static string destination_path;
         public static string[] selected_files;
+        public static List<String> zfn = new List<String>();
+        public static string file_exists_path;
+
+        public static string[] win_fun_fact_list =
+        {
+            "Did you know how to effective work? Try use Snap Windows!",
+            "Have you tried use sticker on your background?",
+            "Have you check on new outlook design? If you didn't try check out!"
+        };
+
+        public static string[] win_fun_fact_list_korean =
+        {
+            "효율적인 작업을 하는 방법을 알고 있나요?, 스냅으로 윈도우 작업해보세요.",
+            "배경화면에 스티커 기능을 사용해보셨나요?",
+            "혹시 새로운 아웃룩 디자인 보셨나요? 한 번 확인해보세요!"
+        };
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             /// <summary>
             /// Select Zipped File Method
             /// </summary>
-
+            zfn.Clear();
+            MultiFileList.Items.Clear();
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "Zip Files (*.zip)|*.zip|7z Files (*.7z)|*.7z|All Files (*.*)|*.*";
             ofd.Title = "Select your zipped file.";
+            ofd.Multiselect = true;
             
             if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                zip_file_name = ofd.FileName;
-                FileN.Text = Path.GetFullPath(zip_file_name);
+                foreach (string file in ofd.FileNames)
+                {
+                    zfn.Add(file);
+                    MultiFileList.Items.Add(Path.GetFileName(file));
+                }
+            }
+            FL.Content = "These File will be extract.";
+            // set file_exists_path to 
+            file_exists_path = Path.GetFullPath(ofd.FileName.Replace(".zip", ""));
+            
+            // if folder path is already exists.
+            if (Directory.Exists((file_exists_path)))
+            {
+                FileAlreadyExists fae = new FileAlreadyExists();
+                fae.Show();
             }
         }
 
@@ -66,66 +99,142 @@ namespace WinZip_11
             if(fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 destination_path = fbd.SelectedPath;
-                FolderN.Text = Path.GetFullPath(destination_path);
+                destination_path = Path.GetFullPath(destination_path);
             }
         }
 
-        private void FolderN_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if(FolderN.Text.ToLower().Equals("con/con") || FolderN.Text.ToLower().Equals("aux/aux") || FolderN.Text.ToLower().Equals("prn/prn"))
-            {
-                NoEaster.Content = "Ayyy!! You found the error of Windows 98!";
-                Secret s = new Secret();
-                s.Show();
-            }
-            else if(FolderN.Text.ToLower().Contains("porn"))
-            {
-                NoEaster.Content = "Police! Police! Arrest this horny person!";
-            }
-            else if (FolderN.Text.ToLower().Contains("egg"))
-            {
-                NoEaster.Content = "Why you storing egg on this computer?";
-            }
-            else
-            {
-                NoEaster.Content = "P.S. There is no easter-egg";
-            }
-        }
-
+        
+        static int count = 0;
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             // TODO Make Extract Function
-            TB1.Header = "     Extract Compressed(Zipped) Folders";
-            Head.Content = "Select a Destination and Extract Files";
-            Body1.Content = "Select compressed file:";
-            Body2.Content = "Files will be extracted to this folder:";
             //Body1.Visibility = Visibility.Visible;
             //Body2.Visibility = Visibility.Visible;
             //FileN.Visibility = Visibility.Visible;
             //FolderN.Visibility = Visibility.Visible;
-            //Button1.Visibility = Visibility.Visible;
-            //Button2.Visibility = Visibility.Visible;
-            //Button3.Visibility = Visibility.Hidden;
-            //Button4.Visibility = Visibility.Hidden;
+            Button1.Visibility = Visibility.Visible;
+            Button2.Visibility = Visibility.Visible;
+            Button3.Visibility = Visibility.Hidden;
+            Button4.Visibility = Visibility.Hidden;
             //TS1.Visibility = Visibility.Visible;
             //this.Height = 523;
+            if(Language.SelectedIndex == 1)
+            {
+                TB1.Header = "    압축 해제 프로그램";
+                Head.Content = "압축 풀 경로와 압축 풀 파일을 선택하세요.";
+                Body1.Content = "압축 파일 선택하세요: ";
+                Body2.Content = "압축 풀 경로를 선택해주세요: ";
+                FL.Content = "Zip 파일을 선택하세요.";
+            }
+            else
+            {
+                TB1.Header = "     Extract Compressed(Zipped) Folders";
+                Head.Content = "Select a Destination and Extract Files";
+                Body1.Content = "Select compressed file:";
+                Body2.Content = "Files will be extracted to this folder:";
+                FL.Content = "Please Select The Zip File.";
+            }
+            count += 1;
+            if(WinFunFact.Content.Equals("Thanks!"))
+            {
+                WinFunFact.Content = "Not again. Why are you doing this?";
+                if(Language.SelectedIndex == 1)
+                {
+                    WinFunFact.Content = "맙소사. 또인가요? 도대체 왜 이러세요?";
+                }
+            }
+            if (count >= 69)
+            {
+                WinFunFact.Content = "did you just feel like boring to you?";
+                if (Language.SelectedIndex == 1)
+                {
+                    WinFunFact.Content = "혹시 지루하시는 건가요?";
+                }
+                if (count >= 75)
+                {
+                    WinFunFact.Content = "I mean... you know I like find some easter egg.";
+                    if (Language.SelectedIndex == 1)
+                    {
+                        WinFunFact.Content = "저는 그니까... 저도 이스터에그를 찾는 거 좋아해요.";
+                    }
+                }
+                if (count >= 80)
+                {
+                    WinFunFact.Content = "Is your work name... Switcher?";
+                    if (Language.SelectedIndex == 1)
+                    {
+                        WinFunFact.Content = "당신의 직업이 스위처인가요?";
+                    }
+                }
+                if (count >= 90)
+                {
+                    WinFunFact.Content = "Stop Switch! You harming your window!";
+                    if (Language.SelectedIndex == 1)
+                    {
+                        WinFunFact.Content = "그만 전환하세요. 당신이 윈도우를 해치고 있어요!";
+                    }
+                }
+            }
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
             // TODO Make Archive Function
-            TB1.Header = "     Archive Files";
-            Head.Content = "Welcome to Archive!";
             //Body1.Visibility = Visibility.Hidden;
-            //Button3.Visibility = Visibility.Visible;
-            //Button4.Visibility = Visibility.Visible;
-            //Button1.Visibility = Visibility.Hidden;
-            //Button2.Visibility = Visibility.Hidden;
+            Button3.Visibility = Visibility.Visible;
+            Button4.Visibility = Visibility.Visible;
+            Button1.Visibility = Visibility.Hidden;
+            Button2.Visibility = Visibility.Hidden;
             //Body2.Visibility = Visibility.Hidden;
             //FileN.Visibility = Visibility.Hidden;
             //FolderN.Visibility = Visibility.Hidden;
             //TS1.Visibility = Visibility.Hidden;
             //this.Height = this.Height - 200;
+            if(Language.SelectedIndex == 1)
+            {
+                TB1.Header = "   파일 압축하기";
+                Head.Content = "파일 압축하는 시스템에 오신걸 환영합니다.";
+                FL.Content = "파일이나 폴더를 선택하시고 압축할 준비를 해주세요.";
+            }
+            else
+            {
+                TB1.Header = "     Archive Files";
+                Head.Content = "Welcome to Archive!";
+                FL.Content = "Please Select File/Folder What you want to archive";
+            }
+            count += 1;
+            if (count >= 69)
+            {
+                WinFunFact.Content = "did you just feel like boring to you?";
+                if(Language.SelectedIndex == 1)
+                {
+                    WinFunFact.Content = "혹시 지루하시는 건가요?";
+                }
+                if (count >= 75)
+                {
+                    WinFunFact.Content = "I mean... you know I like find some easter egg.";
+                    if(Language.SelectedIndex == 1)
+                    {
+                        WinFunFact.Content = "저는 그니까... 저도 이스터에그를 찾는 거 좋아해요.";
+                    }
+                }
+                if (count >= 80)
+                {
+                    WinFunFact.Content = "Is your work name... Switcher?";
+                    if(Language.SelectedIndex == 1)
+                    {
+                        WinFunFact.Content = "당신의 직업이 스위처인가요?";
+                    }
+                }
+                if (count >= 90)
+                {
+                    WinFunFact.Content = "Stop Switch! You harming your window!";
+                    if(Language.SelectedIndex == 1)
+                    {
+                        WinFunFact.Content = "그만 전환하세요. 당신이 윈도우를 해치고 있어요!";
+                    }
+                }
+            }
         }
         
         private void Button3_Click(object sender, RoutedEventArgs e)
@@ -136,6 +245,7 @@ namespace WinZip_11
             {
                 selected_files = ofd.FileNames;
             }
+            FL.Content = "These List will be archive.";
         }
 
         private void Button4_Click(object sender, RoutedEventArgs e)
@@ -156,17 +266,30 @@ namespace WinZip_11
             }
             
         }
-
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
-            ZipFile.ExtractToDirectory(FileN.Text, FolderN.Text);
-            if(TS1.IsChecked == true)
+           
+            for (int i = 0; i < MultiFileList.Items.Count; i++)
             {
-                if(Directory.Exists(FolderN.Text))
+                ZipFile.ExtractToDirectory(Path.GetFullPath(zfn[i]), destination_path);
+            }
+            if (count >= 69)
+            {
+                count = 0;
+                WinFunFact.Content = "Thanks!";
+            }
+            else
+            {
+                count = 0;
+            }
+
+            if (TS1.IsChecked == true)
+            {
+                if(Directory.Exists(destination_path))
                 {
                     ProcessStartInfo startInfo = new ProcessStartInfo
                     {
-                        Arguments = FolderN.Text,
+                        Arguments = destination_path,
                         FileName = "explorer.exe"
                     };
                     Process.Start(startInfo);
@@ -186,6 +309,42 @@ namespace WinZip_11
                 themeService.EnableMica(this, MicaWPF.BackdropType.Tabbed);
             else if (tag_num == 4)
                 themeService.EnableMica(this, MicaWPF.BackdropType.None);
+        }
+
+        private void Language_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(Language.SelectedIndex == 0)
+            {
+                string win_fun = WinFunFact.Content.ToString();
+                if (win_fun.Equals(win_fun_fact_list_korean[0]))
+                {
+                    WinFunFact.Content = win_fun_fact_list[0];
+                }
+                else if (win_fun.Equals(win_fun_fact_list_korean[1]))
+                {
+                    WinFunFact.Content = win_fun_fact_list[1];
+                }
+                else if (win_fun.Equals(win_fun_fact_list_korean[2]))
+                {
+                    WinFunFact.Content = win_fun_fact_list[2];
+                }
+            }
+            else if(Language.SelectedIndex == 1)
+            {
+                string win_fun = WinFunFact.Content.ToString();
+                if (win_fun.Equals(win_fun_fact_list[0]))
+                {
+                    WinFunFact.Content = win_fun_fact_list_korean[0];
+                }
+                else if (win_fun.Equals(win_fun_fact_list[1]))
+                {
+                    WinFunFact.Content = win_fun_fact_list_korean[1];
+                }
+                else if (win_fun.Equals(win_fun_fact_list[2]))
+                {
+                    WinFunFact.Content = win_fun_fact_list_korean[2];
+                }
+            }
         }
     }
 }
