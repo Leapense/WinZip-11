cab833b1c1b304b89e741e46392e44c9f6a8203b
Leapense <Leapense@sillain.ac.kr>
2022-06-23 22:09:50 +0900

    Update Version 1.2

--- WinZip 11/MainWindow.xaml.cs
+++ WinZip 11/MainWindow.xaml.cs
@@ -10,6 +10,9 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
 using System.Windows.Input;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Windows.Media;
 
 namespace WinZip_11
 {
@@ -18,20 +21,26 @@ namespace WinZip_11
     /// </summary>
     public partial class MainWindow : MicaWindow
     {
+        GrammarBuilder grammarBuilder = new GrammarBuilder();
+        Choices commandChoices = new Choices("windows");
+        Choices valueChoices = new Choices();
+        SolidColorBrush brush = new SolidColorBrush();
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
             Loaded += (sender, args) =>
             {
                 
-                WPFUI.Appearance.Watcher.Watch(
+                Wpf.Ui.Appearance.Watcher.Watch(
                   this,                           // Window class
-                  WPFUI.Appearance.BackgroundType.Mica, // Background type
+                  Wpf.Ui.Appearance.BackgroundType.Mica, // Background type
                   true                            // Whether to change accents automatically
                 );
                 Random random = new Random();
                 int tip_num = random.Next(0, win_fun_fact_list.Length);
                 WinFunFact.Content = win_fun_fact_list[tip_num];
+                speech.SelectVoice("Microsoft Zira Desktop");
+                speech.SpeakAsync("Looks like So empty. We're still waiting...");
             };
         }
 
@@ -370,25 +379,8 @@ namespace WinZip_11
 
             }
         }
-
-        private void ProgressRing_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            var secret = new Secret();
-            Random r = new Random();
-            int r_i = r.Next(0, 8);
-            if (r_i >= 7)
-            {
-                secret.ShowDialog();
-                secret = null;
-            }
-            else
-            {
-                FunWithMe.Content = fun_with_progress_ring[r_i];
-                secret = null;
-            }
-            
-        }
-
+        
+        SpeechSynthesizer speech = new SpeechSynthesizer();
         private void ProgressRing_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             var secret = new Secret();
@@ -413,12 +405,193 @@ namespace WinZip_11
                 }
                 secret = null;
             }
+            speech.SelectVoice("Microsoft Zira Desktop");
+            speech.SpeakAsync(FunWithMe.Content.ToString());
         }
 
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {
             Window1 win1 = new Window1();
             win1.Show();
+        }
+
+        private void Speeching_GotFocus(object sender, RoutedEventArgs e)
+        {
+            speech.SelectVoice("Microsoft Zira Desktop");
+            FunWithMe.Content = "I'm listening...";
+            
+            brush.Color = (Color)TryFindResource("SystemAccentColor");
+            RingChan.Foreground = brush;
+            speech.SpeakAsync("I'm listening...");
+        }
+
+        private void Speeching_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            //SolidColorBrush brush = new SolidColorBrush();
+            RingChan.Foreground = Brushes.LightGreen;
+            speech.SelectVoice("Microsoft Zira Desktop");
+            if (e.Key == Key.Enter)
+            {
+                
+                if (Speeching.Text.ToLower().Equals("check option 1"))
+                {
+                    FunWithMe.Content = "I'll check it for you.";
+                    speech.SpeakAsync("I'll check it for you.");
+                    TS2.IsChecked = true;
+                }
+                else if (Speeching.Text.ToLower().Equals("check option 2"))
+                {
+                    FunWithMe.Content = "I'll check it for you.";
+                    speech.SpeakAsync("I'll check it for you.");
+                    TS1.IsChecked = true;
+                }
+                else if (Speeching.Text.ToLower().Equals("uncheck option 1"))
+                {
+                    FunWithMe.Content = "I'll uncheck it for you.";
+                    speech.SpeakAsync("I'll uncheck it for you.");
+                    TS2.IsChecked = false;
+                }
+                else if (Speeching.Text.ToLower().Equals("uncheck option 2"))
+                {
+                    FunWithMe.Content = "I'll uncheck it for you.";
+                    speech.SpeakAsync("I'll uncheck it for you.");
+                    TS1.IsChecked = false;
+                }
+                else
+                {
+                    MoreJoke();
+                }
+                Speeching.Text = "";
+            }
+        }
+
+        private void MoreJoke()
+        {
+            grammarBuilder.Append(commandChoices);
+            string[] command_for_windows =
+            {
+                "95",
+                "98",
+                "2000",
+                "xp",
+                "vista",
+                "7",
+                "8",
+                "10",
+                "11"
+            };
+
+            valueChoices.Add(command_for_windows);
+            
+            string what = Speeching.Text.ToLower();
+            string what_say = "";
+            var smoothy = new Window2();
+            if (what.Equals("never gonna give you up"))
+            {
+                smoothy.Show();
+            }
+            else
+            {
+                string[] what_arr = what.ToLower().Split();
+                List<String> what_list = new List<String>(what_arr);
+                if (what_arr.Contains("windows"))
+                {
+                    int where_start = what_list.IndexOf("windows") + 1;
+                    switch (what_list[where_start])
+                    {
+                        case "95":
+                            FunWithMe.Content = "Windows 95! The Start Button! It was Windows the biggest evolution!";
+                            speech.SpeakAsync("Windows 95! The Start Button! It was Windows the biggest evolution!");
+                            break;
+                        case "98":
+                            FunWithMe.Content = "I was in there, when they launched windows 98 event. and I saw the bluescreen incident.";
+                            speech.SpeakAsync("I was in there, when they launched windows 98 event. and I saw the bluescreen incident.");
+                            break;
+                        case "2000":
+                            FunWithMe.Content = "Windows 2000! I Love the Startup Sound, Are you too?";
+                            speech.SpeakAsync("Windows 2000! I Love the Startup Sound, Are you too?");
+                            break;
+                        case "xp":
+                            FunWithMe.Content = "Windows XP. The Longest Support Windows ever." + Environment.NewLine
+                                + "Have you heard about title dot WMA? " + Environment.NewLine +
+                                "If you didn't, Check out of that. You will love that music";
+                            speech.SpeakAsync("Windows XP. The Longest Support Windows ever.");
+                            speech.SpeakAsync("Have you heard about title dot WMA? If you didn't, Check out of that. You will love that music");
+                            break;
+                        case "vista":
+                            FunWithMe.Content = "Such as Wow, Beautiful Sound, Screen savers! But it was heavy" + Environment.NewLine
+                                + "So that's why people went back to XP.";
+                            speech.SpeakAsync("Such as Wow, Beautiful Sound, Screen savers! But it was heavy.");
+                            speech.SpeakAsync("So that's why people went back to XP.");
+                            break;
+                        case "7":
+                            FunWithMe.Content = "The Perfect Aero! I love that wallpaper.";
+                            speech.SpeakAsync("The Perfect Aero! I love that wallpaper.");
+                            break;
+                        case "8":
+                            FunWithMe.Content = "Windows 8 or 8.1, The First Tablet-like OS and Material Design OS, I love it!";
+                            speech.SpeakAsync("Windows 8 or 8.1, The First Tablet-like OS and Material Design OS, I love it!");
+                            break;
+                        case "10":
+                            FunWithMe.Content = "For long journey, We are enjoy the ride, Don't you agree?";
+                            speech.SpeakAsync("For long journey, We are enjoy the ride, Don't you agree?");
+                            break;
+                        case "11":
+                            FunWithMe.Content = "The Beautiful OS, but some reason getting people makes confusing." + Environment.NewLine
+                                + "I hope this OS makes better to break microsoft Sine Function.";
+                            speech.SpeakAsync("The Beautiful OS, but some reason getting people makes confusing.");
+                            speech.SpeakAsync("I hope this OS makes better to break microsoft Sine Function.");
+                            break;
+                    }
+                }
+                else if (what_arr.Contains("macos"))
+                {
+                    FunWithMe.Content =
+                        "Mac OS? Are you trying to saying about ventura?" + Environment.NewLine
+                        + "Well... I like it their concept. redesignate of old mac os container." + Environment.NewLine
+                        + "In my opinion, The stage manager looks great. except the bug. like you know." + Environment.NewLine
+                        + "If you want to know about siri, here is the joke, cortana is better assistant. heh.";
+                    speech.SpeakAsync("Mac OS? Are you trying to saying about ventura?");
+                    speech.SpeakAsync("Well... I like it their concept. redesignate of old mac os container.");
+                    speech.SpeakAsync("In my opinion, The stage manager looks great. except the bug. like you know.");
+                    speech.SpeakAsync("If you want to know about siri, here is the joke, cortana is better assistant. heh.");
+                }
+                else if (what_arr.Contains("sonic"))
+                {
+                    if (what_arr.Contains("frontier"))
+                    {
+                        FunWithMe.Content = "It's hard to say, But I can tell you." + Environment.NewLine
+                            + "The Music so good and calm.";
+                        speech.SpeakAsync("It's hard to say. But I can tell you.");
+                        speech.SpeakAsync("The Music so good and calm.");
+                        if (what_arr.Contains("animation") || what_arr.Contains("frame"))
+                        {
+                            FunWithMe.Content = "I saw the first ign sonic frontier gameplay. It looks wonderful as open world" +
+                                Environment.NewLine + "But the animation is like something missed when sonic makes big storm attack";
+                            speech.SpeakAsync("I saw the first ign sonic frontier gameplay. It looks wonderful as open world");
+                            speech.SpeakAsync("But the animation is like something missed when sonic makes big storm attack");
+                        }
+                    }
+                }
+                else if (what_arr.Contains("internet explorer") || what_arr.Contains("ie"))
+                {
+                    FunWithMe.Content = "It was the first browser we used. but it's gone. on June 15th, 2022." + Environment.NewLine
+                        + "I wish internet explorer will find some good place on the sky.";
+                    speech.SpeakAsync("It was the first browser we used. but it's gone. on June 15th, 2022.");
+                    speech.SpeakAsync("I wish internet explorer will find some good place on the sky.");
+                }
+                else if (what_arr.Contains("sexual") && what_arr.Contains("joke"))
+                {
+                    speech.SpeakAsync("What?");
+                    speech.SpeakAsync("Are you got horny from me?");
+                    speech.SpeakAsync("I'm sorry, I'm your assistant helper, not sex offender.");
+                }
+                else if(what_arr.Contains("amogus"))
+                {
+                    FunWithMe.Content = "When the imposter was sussy baks.";
+                    speech.SpeakAsync("When the imposter was sussy baka.");
+                }
+            }
         }
     }
 }
---
+++ WinZip 11/Window2.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WinZip_11
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Test.Stop();
+            Test.Dispose();
+        }
+    }
+}
